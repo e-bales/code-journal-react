2ab09d1a5c2086f8a97ec818f2420bf9e59a11f7
Eli Bales <elibales21@gmail.com>
2023-06-30 19:54:08 +0000

    refactored code, started the entries view section

--- client/src/CodeJournal.js
+++ client/src/CodeJournal.js
@@ -9,10 +9,13 @@ export default function CodeJournal() {
   }
 
   return (
-    <NavBar entriesclick={handleEntries} />
-    // <JournalInputs />
-    // <JournalEntries />
-    // <Button />
+    <div className="light-gray">
+      <NavBar entriesclick={handleEntries} />
+      <main>
+        <JournalInputs />
+        <JournalEntries />
+      </main>
+    </div>
   );
 }
 
@@ -39,18 +42,58 @@ function NavBar({ entriesClick }) {
 }
 
 function JournalInputs() {
-  return <form></form>;
+  return (
+    <div className="container" data-view="entry-form">
+      <InputViewTitle text={'New Input'} />
+      <form id="entryForm">
+        <div className="row margin-bottom-1">
+          <Image imgSrc={'CHANGE THIS LATER!'} />
+          <div className="column-half">
+            <TitleInput />
+            <PhotoURLInput />
+          </div>
+        </div>
+        <div className="row margin-bottom-1">
+          <div className="column-full">
+            <NotesInput />
+          </div>
+        </div>
+        <div className="row">
+          <div className="column-full d-flex justify-between">
+            <Button
+              text="DELETE"
+              customClass="invisible delete-entry-button"
+              onCustomClick={'CHANGE THIS LATER!'}
+            />
+            <Button
+              text="SAVE"
+              customClass="input-b-radius text-padding purple-background white-text"
+              onCustomClick={'CHANGE HTIS LATER!'}
+            />
+          </div>
+        </div>
+      </form>
+    </div>
+  );
 }
 
-function Title() {
+function InputViewTitle({ text }) {
+  return (
+    <div className="row">
+      <div className="column-full d-flex justify-between">
+        <h1 id="formH1">{text}</h1>
+      </div>
+    </div>
+  );
+}
+
+function TitleInput() {
   return (
     <>
-      <label class="margin-bottom-1 d-block" for="title">
-        Title
-      </label>
+      <label className="margin-bottom-1 d-block">Title</label>
       <input
         required
-        class="input-b-color text-padding input-b-radius purple-outline input-height margin-bottom-2 d-block width-100"
+        className="input-b-color text-padding input-b-radius purple-outline input-height margin-bottom-2 d-block width-100"
         type="text"
         id="formTitle"
         name="formTitle"
@@ -59,15 +102,13 @@ function Title() {
   );
 }
 
-function PhotoURL() {
+function PhotoURLInput() {
   return (
     <>
-      <label class="margin-bottom-1 d-block" for="photoUrk">
-        Photo URL
-      </label>
+      <label className="margin-bottom-1 d-block">Photo URL</label>
       <input
         required
-        class="input-b-color text-padding input-b-radius purple-outline input-height margin-bottom-2 d-block width-100"
+        className="input-b-color text-padding input-b-radius purple-outline input-height margin-bottom-2 d-block width-100"
         type="text"
         id="formURL"
         name="formURL"
@@ -76,19 +117,65 @@ function PhotoURL() {
   );
 }
 
-function Notes() {
+function Image({ imgSrc }) {
+  return (
+    <div className="column-half">
+      <img
+        className="input-b-radius form-image"
+        id="formImage"
+        src={imgSrc}
+        alt="entry"
+      />
+    </div>
+  );
+}
+
+function NotesInput() {
   return (
     <>
-      <label class="margin-bottom-1 d-block" for="formNotes">
-        Notes
-      </label>
+      <label className="margin-bottom-1 d-block">Notes</label>
       <textarea
         required
-        class="input-b-color text-padding input-b-radius purple-outline d-block width-100"
+        className="input-b-color text-padding input-b-radius purple-outline d-block width-100"
         name="formNotes"
         id="formNotes"
         cols="30"
         rows="10"></textarea>
     </>
   );
+}
+
+function Button({ text, customClass, onCustomClick }) {
+  return (
+    <button className={customClass} type="button" onClick={onCustomClick}>
+      {text}
+    </button>
+  );
+}
+
+function JournalEntries() {
+  return (
+    <div className="container hidden" data-view="entries">
+      <div className="row">
+        <div className="column-full d-flex justify-between align-center">
+          <h1>Entries</h1>
+          <Button
+            text="NEW"
+            customClass="white-text form-link"
+            onCustomClick={'CHANGE THIS LATER!'}
+          />
+        </div>
+      </div>
+      <div className="row">
+        <div className="column-full">
+          <EntryComponentList entries="CHANGE THIS LATER!" />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function EntryComponentList({ entries }) {
+  // const renderedList = entries.map(element => '...');  // some map over our entries...
+  // return (<ul className="entry-ul" id="entryUl">{renderedList}</ul>)
 }
